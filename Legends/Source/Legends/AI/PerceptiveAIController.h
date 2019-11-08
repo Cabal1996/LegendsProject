cfa44607d4cb0729e8dff548afcf18b8fe847979
Leonid <drax1996@gmail.com>
2019-11-08 19:18:58 +0000

    AI Stats Combat

---
+++ Legends/Source/Legends/AI/PerceptiveAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "PerceptiveAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEGENDS_API APerceptiveAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
