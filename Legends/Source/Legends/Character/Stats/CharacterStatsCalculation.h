7bec02301df8e6f47f42c319f3d30791f6c65afc
AtomskACDC <56557049+AtomskACDC@users.noreply.github.com>
2019-11-11 22:25:22 +0200

    StatsClasses

---
+++ Legends/Source/Legends/Character/Stats/CharacterStatsCalculation.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LEGENDS_API CharacterStatsCalculation
+{
+public:
+	CharacterStatsCalculation();
+	~CharacterStatsCalculation();
+};
